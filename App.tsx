6585d22ba9765efcb4dd9f411bf174069e3c0643
dev-sharematch <pachira.dev@gmail.com>
2025-12-13 11:24:17 +0000

    Fix: Restore AI Analysis button to market pages with responsive layout

--- App.tsx
+++ App.tsx
@@ -385,8 +385,13 @@ const App: React.FC = () => {
                     {/* Left Column: Header + Order Book (2/3) */}
                     <div className="flex-[2] flex flex-col min-h-0">
                       {/* Header aligned with order book */}
-                      <div className="flex-shrink-0">
-                        <Header title={getLeagueTitle(activeLeague)} market={activeLeague} />
+                      <div className="flex-shrink-0 mb-4 sm:mb-0">
+                        <div className="flex flex-col-reverse sm:block relative">
+                          <div className="sm:absolute sm:top-0 sm:right-0 sm:z-10 w-full sm:w-auto mt-4 sm:mt-0">
+                            <AIAnalysis teams={teams} leagueName={getLeagueTitle(activeLeague)} />
+                          </div>
+                          <Header title={getLeagueTitle(activeLeague)} market={activeLeague} />
+                        </div>
                       </div>
 
                       {/* Order Book */}
